a6c0d4047b22d04a4f09e1ad5078860bbbe37981
auxibee <auxibee@gmail.com>
2022-01-08 10:42:36 +0000

    added various button types primary,alive,dead etc

--- src/elements/button/button.jsx
+++ src/elements/button/button.jsx
@@ -1,24 +1,49 @@
-import styles from './button.module.css'
-import cn from 'classnames'
+import styles from "./button.module.css";
+import cn from "classnames";
 
-const BaseButton = ({children, ...props}) => {
-    return ( 
-        <button type="button" {...props}> {children} </button>
-     );
-}
+const BaseButton = ({ children, ...rest }) => {
+  return (
+    <button type="button" {...rest}>
+      {children}
+    </button>
+  );
+};
 
-const LinkButton = ({href, children, ...props}) => {
-    return ( 
-        <a href={href} {...props}>{children} </a>
-     );
-}
- 
-const Button = ({children,primary,customStyle, ...props}) => {
-    const HocButton = props.href ? LinkButton : BaseButton
-    return ( 
-        <HocButton className={cn(styles.button, primary && styles.primary, customStyle)} {...props}> {children} </HocButton>
-     );
-}
- 
+const LinkButton = ({ href, children, ...rest }) => {
+  return (
+    <a href={href} {...rest}>
+      {children}
+    </a>
+  );
+};
+
+const Button = ({
+  children,
+  active,
+  notactive,
+  primary,
+  alive,
+  dead,
+  unknown,
+  ...rest
+}) => {
+  const HocButton = rest.href ? LinkButton : BaseButton;
+  return (
+    <HocButton
+      className={cn(
+        styles.button,
+        active && styles.active,
+        notactive && styles.notactive,
+        primary && styles.primary,
+        alive && styles.alive,
+        dead && styles.dead,
+        unknown && styles.unknown
+      )}
+      {...rest}
+    >
+      {children}
+    </HocButton>
+  );
+};
+
 export default Button;
- 
--- src/elements/button/button.stories.js
+++ src/elements/button/button.stories.js
@@ -1,9 +1,20 @@
+
 import Button from './button';
 
 
 export default {
-    title : 'Button',
+    title : 'Elements/Button',
     component: Button
 }
 
-export  const LinkButton = () => <Button href="hello" primary> Link button </Button>
+export  const ActiveLink = () => <Button href="#" active> Active Link </Button>
+
+export  const NonActiveLink = () => <Button href="#" notactive> Normal Link </Button>
+
+export const PrimaryButton = () => <Button primary> Search </Button>
+
+export const AliveButton = () => <Button primary alive> Alive </Button>
+
+export const DeadButton = () => <Button primary dead> Dead </Button>
+
+export const UnknownButton = () => <Button primary unknown> Unknown </Button>
