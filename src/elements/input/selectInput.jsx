c6727a3d0da33194c063ffca6d5bc48dcddb8ba7
auxibee <auxibee@gmail.com>
2022-01-15 08:39:57 +0000

    refactored

--- src/elements/input/selectInput.jsx
+++ src/elements/input/selectInput.jsx
@@ -1,13 +1,21 @@
 import styles from "./input.module.css";
 
-const SelectInput = ({ name, options }) => {
+const SelectInput = ({ name, options, onChange }) => {
   return (
-    <select name={name} id={name} className={styles.selectInput}>
-      {options.map((option) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
+    <select
+      name={name}
+      id={name}
+      className={styles.selectInput}
+      onChange={onChange}
+    >
+      <option value={1}>Choose....</option>
+      {[...Array(options).keys()].map((x) => {
+        return (
+          <option key={x + 1} value={x + 1}>
+            {name} - {x + 1}
+          </option>
+        );
+      })}
     </select>
   );
 };
