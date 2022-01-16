704f11dfa03748ca309bd19d3c1a4ba1254940a7
auxibee <auxibee@gmail.com>
2022-01-16 16:32:23 +0000

    added the pagination component

--- src/pages/characters/characters.jsx
+++ src/pages/characters/characters.jsx
@@ -7,19 +7,65 @@ import { useEffect, useState } from "react";
 import { fetchAxios } from "./../api";
 
 import styles from "../styles.module.css";
-
-const sample = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-];
+import Accordion from "components/accordion/accordion";
+import usePagination from "components/pagination/usePagination";
+import Pagination from "components/pagination/pagination";
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  const [filter, setFilter] = useState("");
+  let [currentPage, setCurrentPage] = useState(1);
 
+  const totalCount = 826;
+  const pageSize = 20;
+
+  const [firstPage, currPage, lastPage] = usePagination({
+    totalCount,
+    pageSize,
+    currentPage,
+  });
+
+  const btnNext = currentPage === lastPage ? true : false;
+  const btnPrevious =
+    currentPage === firstPage || currentPage === 1 ? true : false;
+
+  const handlePageNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePagePrev = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const filterItems = [
+    { title: "Status", buttons: ["alive", "dead", "unknown"] },
+    {
+      title: "Species",
+      buttons: [
+        "Human",
+        "Alien",
+        "Humanoid",
+        "Poopybutthole",
+        "Mythological",
+        "Unknown",
+        "Animal",
+        "Disease",
+        "Robot",
+        "Cronenberg",
+        "Planet",
+      ],
+    },
+    { title: "Gender", buttons: ["female", "male", "genderless", "unknown"] },
+  ];
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value.toLowerCase());
+  };
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetchAxios(
-          `https://rickandmortyapi.com/api/character/?page=2`
+          `https://rickandmortyapi.com/api/character/?page=${currentPage}&${filter}`
         );
 
         const { data } = response;
@@ -41,7 +87,7 @@ const Characters = () => {
     };
 
     fetchData();
-  }, []);
+  }, [filter, currentPage]);
 
   return (
     <div className={styles.main}>
@@ -56,9 +102,21 @@ const Characters = () => {
       </section>
       <aside>
         <h3>Filters</h3>
-        <p>clear Filters</p>
+        <Accordion items={filterItems} onFilter={handleFilter} />
       </aside>
       <div className={styles.content}>{<WikiList results={characters} />}</div>
+      <footer>
+        {currPage && (
+          <Pagination
+            currentPage={currentPage}
+            lastPage={lastPage}
+            onPageNext={handlePageNext}
+            onPagePrevious={handlePagePrev}
+            nextBtnState={btnNext}
+            prevBtnState={btnPrevious}
+          />
+        )}
+      </footer>
     </div>
   );
 };
