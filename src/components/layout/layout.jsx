8d80e58db67ad1ee2bab702427881c90027ec52a
auxibee <auxibee@gmail.com>
2022-01-23 16:12:03 +0000

    changed the styling

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,12 +1,18 @@
 import Header from "components/header/header";
 
 import styles from "./layout.module.css";
+import { useState } from "react";
 
 const Layout = ({ children }) => {
+  const [selected, setSelected] = useState("characters");
   return (
     <div className={styles.layout}>
       <header>
-        <Header />
+        <Header
+          buttons={["characters", "location", "episodes"]}
+          selected={selected}
+          setSelected={setSelected}
+        />
       </header>
       <main>{children}</main>
     </div>
