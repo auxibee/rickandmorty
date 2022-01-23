42d2ffe5fba58fdc64bd8ce1c3f783ddfc892b0d
auxibee <auxibee@gmail.com>
2022-01-23 16:10:16 +0000

    changed the styling of select input

--- src/elements/input/input.jsx
+++ src/elements/input/input.jsx
@@ -1,9 +1,11 @@
 import styles from "./input.module.css";
 
-const SearchInput = () => {
+const SearchInput = ({ value, onInputChange }) => {
   return (
     <input
       className={styles.searchInput}
+      value={value}
+      onChange={onInputChange}
       type="text"
       placeholder="Search for characters"
     />
--- src/elements/input/input.stories.js
+++ src/elements/input/input.stories.js
@@ -1,13 +1,18 @@
 
 import SearchInput from './input';
 import SelectInput from './selectInput';
+import { useState } from 'react';
 
 export default {
     title : 'Elements/Inputs'
 }
 
-export const searchInput = () => <SearchInput />
-
+export const searchInput = () => {
+    const [inputValue, setInputValue] = useState('')
+    return (
+        <SearchInput value={inputValue} onInputChange={setInputValue}/>
+    )
+}
 
 const options = ["Choose....","episode-1","espisode-2", "episode-3"]
 export const SelectionInput = () => <SelectInput name="episodes" options={options}/>
